8da977c7ffba248b5c2976c94b27ccfb2bf1dfbe
kwansing14 <kwansing@hotmail.sg>
2023-12-28 01:02:53 +0800

    feat: ads.txt

---
+++ src/app/learnmore/page.tsx
@@ -0,0 +1,24 @@
+<div>
+  Privacy Policy This Privacy Policy explains how we collect, use, and disclose
+  information collected from visitors to our website in connection with the
+  AdSense program. By using this website, you agree to the terms of this Privacy
+  Policy. Information Collection and Use We may collect certain information
+  automatically, including but not limited to: Log Data: When you visit our
+  website, our servers automatically record information that your browser sends
+  whenever you visit a website. Cookies: We may use cookies to collect
+  information and improve our services. You can instruct your browser to refuse
+  all cookies or to indicate when a cookie is being sent. Google AdSense We use
+  Google AdSense to serve ads on our website. Google uses cookies to help serve
+  relevant ads to users based on their visits to this and other websites. Users
+  may opt out of personalized advertising by visiting Google's Ads Settings.
+  Third-Party Links Our website may contain links to third-party websites.
+  Please note that we have no control over the content, privacy policies, or
+  practices of any third-party sites or services. Data Security We implement
+  appropriate security measures to protect against unauthorized access,
+  alteration, disclosure, or destruction of your personal information. Changes
+  to This Privacy Policy We reserve the right to update or change our Privacy
+  Policy at any time. Any changes will be effective immediately upon posting the
+  updated Privacy Policy on this page. Contact Us If you have any questions or
+  suggestions about our Privacy Policy, please contact us at [your contact
+  information].
+</div>;
